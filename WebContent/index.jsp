72d833ee079a594eeb286d4ea3074c0443e3e48a
misterdan <misterdan@bigmir.net>
2014-04-07 21:43:55 +0300

    Test excel

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,5 +1,13 @@
-<jsp:include page="../WEB-INF/jsphf/header.jsp" />
-
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Ziphyrus</title>
+    </head>
+    <body>
 Main index page
-
-<jsp:include page="../WEB-INF/jsphf/footer.jsp" />
+${country}
+    </body>
+</html>
--- src/com/zephyrus/wind/MainServlet.java
+++ src/com/zephyrus/wind/MainServlet.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,7 @@ import com.zephyrus.wind.managers.MessageManager;
 
 // The Main entry point
 
-@WebServlet("/MainServlet")
+
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
