962111469f1248a354b51152bdb3d4902c0dd41a
Igor <zzzeeerrr0@gmail.com>
2014-04-28 08:26:38 +0300

    Added documentation to findFreePortID in IPortDAO

--- src/com/zephyrus/wind/dao/interfaces/IPortDAO.java
+++ src/com/zephyrus/wind/dao/interfaces/IPortDAO.java
@@ -6,7 +6,14 @@ import com.zephyrus.wind.model.Port;
 import com.zephyrus.wind.model.Task;
 																							// REVIEW: documentation expected
 public interface IPortDAO extends IDAO<Port> {
-
+	
+	/**
+	 * Method for searching ID of fist free port (without cable)
+	 * 
+	 * @see com.zephyrus.wind.dao.interfaces.ICableDAO.getCableDAO
+	 * @return ID of first free port or 0 if it doesn't exist
+	 * @author Miroshnychenko Nataliya
+	 */
 	int findFreePortID() throws Exception;
 
 	Port findPortFromTaskID(Task task) throws Exception;
