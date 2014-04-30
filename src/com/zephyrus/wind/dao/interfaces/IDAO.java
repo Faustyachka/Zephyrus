259b2e6aac62e33b2e547af43f2ed70d448edfff
Miroshnychenko Nataliya <nataliya.miroshnychenko@gmail.com>
2014-04-30 22:25:39 +0300

    Add methods find(int, int) and count() for pagination

--- src/com/zephyrus/wind/dao/interfaces/IDAO.java
+++ src/com/zephyrus/wind/dao/interfaces/IDAO.java
@@ -1,6 +1,7 @@
 package com.zephyrus.wind.dao.interfaces;
 
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -8,9 +9,25 @@ import java.util.ArrayList;
  * @author MrDan																						// REVIEW: who is it?
  */
 public interface IDAO<T> {																				// REVIEW: unchecked DAO exception should be used
-    ArrayList<T> findAll() throws Exception;															// REVIEW: method forbidden. Paging should be used
-    T findById(int id) throws Exception;
-    T findByRowId(String id) throws Exception;															// REVIEW: findByRowId? What is has to do with DAO?
+	@Deprecated
+	ArrayList<T> findAll() throws Exception;															// REVIEW: method forbidden. Paging should be used
+	
+    /**
+	 * Method find specified count of class objects
+	 * 
+	 * @param int firstItem - begin item of search
+	 * @param int count - number of return class objects
+	 * @return ArrayList<T> - collection of class objects
+	 */
+	public ArrayList<T> find(int firstItem, int count) throws Exception;
+	
+    /**
+	 * Method find count of existing class objects from corresponding DB table
+	 * 
+	 * @return int - number of existing class objects
+	 */
+	public int count() throws SQLException;
+	T findById(int id) throws Exception;
     void update(T record) throws Exception;															
     T insert(T record) throws Exception;
     int remove(T record) throws Exception;
