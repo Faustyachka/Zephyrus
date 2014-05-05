9611506113b3d03abeccf4e2ca8cf9c9e4381fb0
Igor <zzzeeerrr0@gmail.com>
2014-05-05 19:17:35 +0300

    Updated ModifyWorkflow - added circuit configuration

--- src/com/zephyrus/wind/workflow/ModifyScenarioWorkflow.java
+++ src/com/zephyrus/wind/workflow/ModifyScenarioWorkflow.java
@@ -1,10 +1,20 @@
 package com.zephyrus.wind.workflow;
 
 import com.zephyrus.wind.dao.factory.OracleDAOFactory;
+import com.zephyrus.wind.dao.interfaces.ICircuitDAO;
+import com.zephyrus.wind.dao.interfaces.IDeviceDAO;
+import com.zephyrus.wind.dao.interfaces.IPortDAO;
+import com.zephyrus.wind.dao.interfaces.IPortStatusDAO;
 import com.zephyrus.wind.dao.interfaces.IServiceInstanceDAO;
 import com.zephyrus.wind.enums.ORDER_STATUS;
 import com.zephyrus.wind.enums.ORDER_TYPE;
+import com.zephyrus.wind.enums.PORT_STATUS;
+import com.zephyrus.wind.enums.ROLE;
 import com.zephyrus.wind.enums.SERVICEINSTANCE_STATUS;
+import com.zephyrus.wind.model.Circuit;
+import com.zephyrus.wind.model.Device;
+import com.zephyrus.wind.model.Port;
+import com.zephyrus.wind.model.PortStatus;
 import com.zephyrus.wind.model.ServiceInstance;
 import com.zephyrus.wind.model.ServiceOrder;
 
@@ -29,12 +39,6 @@ public class ModifyScenarioWorkflow extends Workflow {
 		if (order.getOrderType().getId() != ORDER_TYPE.MODIFY.getId()) {
             throw new WorkflowException("Cannot proceed Order: wrong order scenario");
         }
-		
-		ServiceInstance si = order.getServiceInstance();
-		if(si.getServInstanceStatus().getId() != SERVICEINSTANCE_STATUS.ACTIVE.getId()) {
-			throw new WorkflowException("Service Instance associated with current Order "
-					+ "is not Active");
-		}
     }
     
     /**
@@ -58,16 +62,51 @@ public class ModifyScenarioWorkflow extends Workflow {
     		}
             
             changeOrderStatus(ORDER_STATUS.PROCESSING);
+            createTask(ROLE.PROVISION);
+        } catch (Exception exc) {
+        	throw new WorkflowException("Exception while proceeding order", exc);
+		} finally {
+			lock.unlock();
+		}
+    }
+    
+    /**
+     * This method allows Provisioning Engineer to update Circuit configuration
+     * as part of the Modify scenario workflow. Note that this action automatically
+     * completes Order. It also updates SI date because of the billing that should
+     * now be performed differently due to service price change.
+     * @param taskID ID of task for Provisioning Engineer
+     * @param circuitConfig configuration of circuit
+     */
+    public void updateCircuitConfig(int taskID, String circuitConfig) {
+    	lock.lock();
+    	try {
+            if (!isTaskValid(taskID, ROLE.PROVISION.getId())) {
+                throw new WorkflowException("Given Task is not valid");
+            }
+            
+            if(circuitConfig == null) {
+            	throw new WorkflowException("Wrong circuit config");
+            }
             
             // Change product ID for Service Instance
+            ServiceInstance si = order.getServiceInstance();
             IServiceInstanceDAO siDAO = factory.getServiceInstanceDAO();
             si.setProductCatalog(order.getProductCatalog());
             siDAO.update(si);
             
+            // Update circuit config
+            Circuit circuit = si.getCircuit();
+            ICircuitDAO circuitDAO = factory.getCircuitDAO();
+            circuit.setConfig(circuitConfig);
+            circuitDAO.update(circuit);
+            
+            completeTask(taskID);
+
             updateServiceInstanceDate(order.getServiceInstance());
             this.completeOrder();
         } catch (Exception exc) {
-        	throw new WorkflowException("Exception while proceeding order", exc);
+			throw new WorkflowException("Circuit configuration failed", exc);
 		} finally {
 			lock.unlock();
 		}
