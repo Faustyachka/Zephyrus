cd00d9dbbcd7f5b31a4fa7eab73edb96fbfa9689
zzzeeerrr0 <zzzeeerrr0@gmail.com>
2014-05-19 18:29:57 +0300

    Added docs to Command interfaces

--- src/com/zephyrus/wind/commands/interfaces/Command.java
+++ src/com/zephyrus/wind/commands/interfaces/Command.java
@@ -2,8 +2,20 @@ package com.zephyrus.wind.commands.interfaces;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-																									// REVIEW: documentation expected
+
+/** 
+ * This interface provides generic method for Commands' execution
+ */
 public interface Command {
+	
+	/**
+	 * This generic method allows to execute current Command with
+	 * given request/response params of servlet that invoked this method
+	 * @param request instance of HttpServletRequest, which relays given request params
+	 * @param response instance of HttpServletResponse, which allows to relay response
+	 * @return string, representing page user should be redirected to after
+	 * method execution
+	 */
 	public String execute(HttpServletRequest request, HttpServletResponse response)
             throws Exception;
 }
--- src/com/zephyrus/wind/commands/interfaces/SQLCommand.java
+++ src/com/zephyrus/wind/commands/interfaces/SQLCommand.java
@@ -8,40 +8,52 @@ import javax.servlet.http.HttpServletResponse;
 import com.zephyrus.wind.dao.factory.OracleDAOFactory;
 import com.zephyrus.wind.enums.PAGES;
 import com.zephyrus.wind.managers.MessageManager;
-																													// REVIEW: documentation & author expected
+
+/**
+ * This class implements generic methods, used to manage transactions
+ * and concurrency of Commands' execution method invocations.
+ */
 public abstract class SQLCommand implements Command {
-	private static ThreadLocal<OracleDAOFactory> oracleFactoryThreadLocal = new ThreadLocal<OracleDAOFactory>(){	// REVIEW: too long line
+	private static ThreadLocal<OracleDAOFactory> oracleFactoryThreadLocal = new ThreadLocal<OracleDAOFactory>() {	
 		public OracleDAOFactory initialValue(){
 			return new OracleDAOFactory();
 		}
 	};
 
-  public SQLCommand() {																								// REVIEW: empty constructor. is it necessary? 
-  }
-  																													// REVIEW: Override annotation & documentation expected
-  public String execute(HttpServletRequest request, HttpServletResponse response)
-          throws Exception {
-      String page = null;
-      try {
-    	  getOracleDaoFactory().beginConnection();
-          page = doExecute(request, response);
-          getOracleDaoFactory().commitTransaction();
-      } catch(SQLException ex){
-          ex.printStackTrace();
-          request.setAttribute("errorMessage", MessageManager.SQL_ERROR_MESSAGE + ex.getMessage());
-          request.setAttribute("title", MessageManager.ERROR);
-          page = PAGES.MESSAGE_PAGE.getValue();
-      } finally{
-    	  getOracleDaoFactory().endConnection();
-    	  oracleFactoryThreadLocal.remove();
-      }
-      return page;
-  }
+	public SQLCommand() {	}
+  		
+     /**
+	  * This generic method allows to execute current Command with
+	  * given request/response params of servlet that invoked this method
+	  * @param request instance of HttpServletRequest, which relays given request params
+	  * @param response instance of HttpServletResponse, which allows to relay response
+	  * @return string, representing page user should be redirected to after
+	  * method execution
+	  */
+	  @Override
+	  public String execute(HttpServletRequest request, HttpServletResponse response)
+	          throws Exception {
+	      String page = null;
+	      try {
+	    	  getOracleDaoFactory().beginConnection();
+	          page = doExecute(request, response);
+	          getOracleDaoFactory().commitTransaction();
+	      } catch(SQLException ex){
+	          ex.printStackTrace();
+	          request.setAttribute("errorMessage", MessageManager.SQL_ERROR_MESSAGE + ex.getMessage());
+	          request.setAttribute("title", MessageManager.ERROR);
+	          page = PAGES.MESSAGE_PAGE.getValue();
+	      } finally{
+	    	  getOracleDaoFactory().endConnection();
+	    	  oracleFactoryThreadLocal.remove();
+	      }
+	      return page;
+	  }
   
-  protected OracleDAOFactory getOracleDaoFactory(){
-	  return oracleFactoryThreadLocal.get();
-  }
-  
-  protected abstract String doExecute(HttpServletRequest request, HttpServletResponse response)
-          throws SQLException, Exception;
+	  protected OracleDAOFactory getOracleDaoFactory(){
+		  return oracleFactoryThreadLocal.get();
+	  }
+	  
+	  protected abstract String doExecute(HttpServletRequest request, HttpServletResponse response)
+	          throws SQLException, Exception;
 }
